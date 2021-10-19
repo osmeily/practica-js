c391bd1b3cdf0468fda7ff361627f9a2a018ebfd
Osmeily <osmeilypernia@gmail.com>
2021-10-19 17:44:50 -0500

    First commit

---
+++ ejercicio1/ejercicio1.js
@@ -0,0 +1,11 @@
+/** Ejercicio #1
+ *  Crear un algoritmo que calcule el precio final de un producto, teniendo
+ *  en cuenta el iva del 19%, el usuario debe ingresar el valor del producto
+ *  sin iva y el algoritmo debe retornar el valor final.
+ */
+
+let valor = Number(prompt("Ingrese el precio:"))
+let iva = 19
+let valorFinal = (valor * (iva/100)) + valor
+
+document.write(valorFinal)
